93508ccc053f4c1366d05c3973f6648797ca3c41
DankoMaksym <dankomaksym@gmail.com>
2020-09-30 14:43:53 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,73 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machine = true) {
+    this.machine = machine;
+  }
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("THROWN");
+    }
+
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    message = message.toUpperCase();
+    this.machine ? message : (message = message.split("").reverse().join(""));
+
+    key = key.toUpperCase();
+    let n = key.length;
+
+    let counter = 0;
+    let newMessage = "";
+    while (counter < message.length) {
+      for (let i = 0; i < n; i++) {
+        if (counter >= message.length) break;
+        if (alphabet.indexOf(message[counter]) < 0) {
+          newMessage += message[counter];
+          i = i - 1;
+        } else {
+          newMessage +=
+            alphabet[
+              (alphabet.indexOf(message[counter]) + alphabet.indexOf(key[i])) %
+                26
+            ];
+        }
+        counter += 1;
+      }
+    }
+    return newMessage;
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("THROWN");
+    }
+
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    message = message.toUpperCase();
+    this.machine ? message : (message = message.split("").reverse().join(""));
+    key = key.toUpperCase();
+    let n = key.length;
+
+    let counter = 0;
+    let newMessage = "";
+    while (counter < message.length) {
+      for (let i = 0; i < n; i++) {
+        if (counter >= message.length) break;
+        if (alphabet.indexOf(message[counter]) < 0) {
+          newMessage += message[counter];
+          i = i - 1;
+        } else {
+          newMessage +=
+            alphabet[
+              (alphabet.indexOf(message[counter]) -
+                alphabet.indexOf(key[i]) +
+                26) %
+                26
+            ];
+        }
+        counter += 1;
+      }
+    }
+    return newMessage;
   }
 }
 
